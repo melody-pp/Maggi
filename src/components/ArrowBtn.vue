7ac1169e20cbd264c4b321ee4f11541f4c02b612
王跃 <wangyue57@meituan.com>
2018-01-11 17:56:49 +0800

    arrow animate

--- src/components/ArrowBtn.vue
+++ src/components/ArrowBtn.vue
@@ -1,8 +1,27 @@
 <template>
   <div class="arrowUp">
-    <img src="../assets/index/001.png" alt="">
+    <img ref="arrow" src="../assets/index/001.png" alt="">
   </div>
 </template>
+
+<script>
+  import { tween, styler, easing } from 'popmotion'
+
+  export default {
+    mounted () {
+      const $arrow = this.$refs.arrow
+      const arrowStyler = styler($arrow)
+
+      tween({
+        to: 15,
+        ease: easing.easeInOut,
+        flip: Infinity,
+        duration: 500
+      }).start(arrowStyler.set('y'))
+    }
+  }
+</script>
+
 <style scoped lang="scss">
   .arrowUp {
     position: absolute;
