fe8c3cbef3dd7f4eba7dd3bdaa707f7ce34ab730
wangyue <hust_wangyue@foxmail.com>
2018-01-14 18:23:16 +0800

    关注公众号打勾

--- src/pages/prize/Prize.vue
+++ src/pages/prize/Prize.vue
@@ -2,21 +2,20 @@
   <div>
     <div class="prizeBox">
       <marquee behavior="scroll" direction="left">
-      <img src="../../assets/prize/Warm-air-blower.png" alt="">
-      <img src="../../assets/prize/Vacuum-cup.png" alt="">
-      <img src="../../assets/prize/Intelligent-bracelet.png" alt="">
-      <img src="../../assets/prize/Charging.png" alt="">
-      <img src="../../assets/prize/Electronic-gift-card.png" alt="">
-      <img src="../../assets/prize/The-cook.png" alt="">
+        <img src="../../assets/prize/Warm-air-blower.png" alt="">
+        <img src="../../assets/prize/Vacuum-cup.png" alt="">
+        <img src="../../assets/prize/Intelligent-bracelet.png" alt="">
+        <img src="../../assets/prize/Charging.png" alt="">
+        <img src="../../assets/prize/Electronic-gift-card.png" alt="">
+        <img src="../../assets/prize/The-cook.png" alt="">
       </marquee>
     </div>
     <img class="rules" src="../../assets/prize/Sweepstakes-rules.png" alt="">
     <div>
-      <div class="agreenBox clearfix">
-
-        <span class="agreenBtn" @click=""></span>
+      <div class="agreeBox clearfix">
+        <span :class="['agreeBtn',{follow}]" @click="follow=!follow"></span>
         <span style="margin-right: 17vw;">关注公众号</span>
-        <span class="agreenBtn" @click=""></span>
+        <span :class="['agreeBtn',{knowRule}]" @click="knowRule=!knowRule"></span>
         <span @click="showDialog">我已了解活动规则</span>
       </div>
     </div>
@@ -60,12 +59,14 @@
 </template>
 <script>
   export default {
-    data(){
-      return{
+    data () {
+      return {
         dialogVisible: false,
+        follow: true,
+        knowRule:true
       }
     },
-    methods:{
+    methods: {
       showDialog () {
         this.dialogVisible = true
       },
@@ -88,12 +89,14 @@
     }
 
   }
+
   .close {
     width: 6.67vw;
     position: absolute;
     top: -10vh;
     right: 2vw;
   }
+
   .theRules {
     width: 17.6vw;
     position: absolute;
@@ -116,17 +119,20 @@
     }
   }
 
-  .agreenBox {
+  .agreeBox {
     margin-top: 4vh;
-    .agreenBtn {
+    .agreeBtn {
       display: inline-block;
       width: 5.33vw;
       height: 3.15vh;
-      background: url("../../assets/leaveInfo/reading.png") no-repeat 0 -3.15vh;
+      background: url("../../assets/leaveInfo/reading.png") no-repeat 0 0;
       background-size: 5.33vw 6.3vh;
       cursor: pointer;
       margin-right: 2vw;
       vertical-align: -6px;
+      &.follow, &.knowRule {
+        background-position: 0 -3.15vh;
+      }
     }
   }
 </style>
