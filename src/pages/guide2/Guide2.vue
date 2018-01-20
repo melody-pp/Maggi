fdeb4a0bc9f5b937438c1e45618d688fefd5ca43
wangyue <hust_wangyue@foxmail.com>
2018-01-21 02:01:34 +0800

    animate

--- src/pages/guide2/Guide2.vue
+++ src/pages/guide2/Guide2.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div ref="page">
     <div class="bg-container">
       <div class="topImg clearfix">
         <img src="../../assets/p2/01.jpg" ref="img1">
@@ -16,14 +16,12 @@
     </div>
 
     <ArrowBtn v-show="showArrow"/>
-
-    <transition enter-active-class="animated zoomIn">
-      <Modal :z-index="2" v-show="showModal"/>
-    </transition>
+    <Modal :z-index="2"/>
   </div>
 </template>
 
 <script>
+  import { TimelineMax } from 'gsap'
   import Modal from '../../components/Modal'
   import ArrowBtn from '../../components/ArrowBtn'
 
@@ -32,12 +30,34 @@
     components: {Modal, ArrowBtn},
     data () {
       return {
+        timeline: null,
         showArrow: true,
-        showModal: true,
+      }
+    },
+    methods: {
+      animate () {
+        this.timeline = new TimelineMax({
+          delay: 0.7,
+          onComplete: () => {
+            this.showArrow = true
+          }
+        })
+
+        this.timeline
+          .from(this.$refs.img1, 1, {autoAlpha: 0, x: 100, y: 100})
+          .from(this.$refs.img2, 1, {autoAlpha: 0, x: -100, y: 100}, '-=1')
+          .from(this.$refs.img3, 1, {autoAlpha: 0, x: 100, y: -100}, '-=1')
+          .from(this.$refs.img4, 1, {autoAlpha: 0, x: -100, y: -100}, '-=1')
+          .from(this.$refs.bottomImg, 1, {autoAlpha: 0, scale: 2})
+          .from(this.$refs.page.querySelector('.modal'), 1, {autoAlpha: 0, scale: 0})
+          .from(this.$refs.title, 1, {autoAlpha: 0, y: -50})
+          .from(this.$refs.subTitle, 1, {autoAlpha: 0, y: -50})
+          .from(this.$refs.content, 1, {autoAlpha: 0, y: -50})
       }
     },
     watch: {
       moveIn (newVal) {
+        newVal && this.animate()
       }
     },
   }
