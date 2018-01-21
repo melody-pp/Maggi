9f5b7394a6bb8389d45da0b19ea470d05e2ff5da
wangyue <hust_wangyue@foxmail.com>
2018-01-21 13:40:20 +0800

    close dialog

--- src/pages/personalInfo/PersonalInfo.vue
+++ src/pages/personalInfo/PersonalInfo.vue
@@ -52,7 +52,7 @@
         <p>8. 本活动严禁刷票，若有违反者，雀巢专业餐饮有权立刻取消其参与资格。</p>
         <p>9. 本活动解释权归雀巢专业餐饮所有。</p>
       </div>
-      <button>已了解活动规则</button>
+      <button @click="dialogVisible=false">已了解活动规则</button>
     </el-dialog>
   </div>
 </template>
